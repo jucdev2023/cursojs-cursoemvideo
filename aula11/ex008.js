1bda507569b5bbd64b7bb34c128598339578203d
Juliana Silva <jujucdev2525@outlook.com>
2023-10-09 14:39:15 -0300

    curso de javaScript/prof guanabara

---
+++ aula11/ex008.js
@@ -0,0 +1,12 @@
+// esses comandos são comando sequenciais, não existe a possibilidade de eu executar apenas um comando especifico
+//SE EU fazer dessa forma, eu vou sempre multar a pessoa. vamos supor que a velocidade  que ele passoou é 12, mas no dódigo ta escrito multado, ele sempre vai ser multado, idependente da velocidade
+
+// if ( ) -> leia assim "se por acaso"
+var vel = 178.2
+console.log(` a velocidade do seu carro é ${vel} km/h por hora`)
+
+if (vel > 60) { // condição simples
+    console.log(`voce ultrapassou a velocidade permitida. MULTADO!!!`)
+} 
+console.log(`sempre dirija usando sinto de segurança`)
+
---
+++ aula11/ex009.js
@@ -0,0 +1,10 @@
+// condição composta
+
+var pais = 'EUA'
+console.log(`vivendo em ${pais}`)
+if (pais=='Brasil') {
+  console.log('Brasileiro')
+
+} else {
+    console.log('você é Estrangeiro')
+}
